4b9bfe5b12d6a597736c1a90cbb26ef8863d09e5
@Doniyor_U <doniyor.usmonov@ssd.uz>
2022-04-28 08:25:28 +0500

    asa

---
+++ src/js/components/swiper.js
@@ -0,0 +1,32 @@
+// Подключение свайпера
+import Swiper, {Navigation, Pagination} from 'swiper';
+
+Swiper.use([Navigation, Pagination]);
+const swiper = new Swiper(".mySwiper2", {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+const slider = document.querySelector('.swiper-wrapper')
+
+
+slider.addEventListener('slideChange', function (realIndex) {
+  if (document.querySelector('.swiper-slide.swiper-slide-active').classList.contains('dark')) {
+    document.querySelector('.swiper__info-boxes').classList.add('active')
+  } else {
+    document.querySelector('.swiper__info-boxes').removeClass('active');
+  }
+});
+
+
+
+
