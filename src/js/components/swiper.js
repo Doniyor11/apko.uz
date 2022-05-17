b8bea891b56ebdbde535ce72ff64636a410db758
Doniyor11 <udoniyorbek99@mail.ru>
2022-05-17 10:38:53 +0500

    s

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -3,6 +3,7 @@ import Swiper, {Navigation, Pagination} from 'swiper';
 
 Swiper.use([Navigation, Pagination]);
 const swiper = new Swiper(".mySwiper2", {
+
   // Optional parameters
   direction: 'horizontal',
   loop: true,
@@ -13,19 +14,29 @@ const swiper = new Swiper(".mySwiper2", {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
+  on: {
+    init: function () {
+      console.log('swiper initialized');
+    },
+  },
 });
 
-const slider = document.querySelector('.swiper-wrapper')
 
-
-slider.addEventListener('slideChange', function (realIndex) {
-  if (document.querySelector('.swiper-slide.swiper-slide-active').classList.contains('dark')) {
-    document.querySelector('.swiper__info-boxes').classList.add('active')
-  } else {
-    document.querySelector('.swiper__info-boxes').removeClass('active');
-  }
+swiper.on('slideChange', function () {
+  console.log('slide changed');
 });
+
+// const slider = document.querySelector('.swiper-wrapper')
+
+
+// slider.addEventListener('change', function (realIndex) {
+//   console.log('ok')
+//   if (document.querySelector('.swiper-slide.swiper-slide-active').classList.contains('dark')) {
+//     document.querySelector('.swiper__info-boxes').classList.add('active')
+//   } else {
+//     document.querySelector('.swiper__info-boxes').removeClass('active');
+//   }
+// });
 
 
 
